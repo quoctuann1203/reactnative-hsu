9db00bf4ad454c8d446a5787d04e859a0c04604e
Quoc Tuan <you@example.com>
2022-04-12 11:32:20 +0700

    week 3 - react navigation- finish

--- component/MainComponent.js
+++ component/MainComponent.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from './HomeComponent';
+function HomeNavigatorScreen() {
+  const HomeNavigator = createStackNavigator();
+  return (
+    <HomeNavigator.Navigator
+      initialRouteName='Home'
+      screenOptions={{
+        headerStyle: { backgroundColor: '#512DA8' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { color: '#fff' }
+      }}>
+      <HomeNavigator.Screen name='Home' component={Home} />
+    </HomeNavigator.Navigator>
+  );
+}
 
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
@@ -20,11 +36,21 @@ function MenuNavigatorScreen() {
   );
 }
 
+function MainNavigatorScreen() {
+  const MainNavigator = createDrawerNavigator();
+  return (
+    <MainNavigator.Navigator initialRouteName='HomeScreen'>
+      <MainNavigator.Screen name='HomeScreen' component={HomeNavigatorScreen} options={{ title: 'Home', headerShown: false }} />
+      <MainNavigator.Screen name='MenuScreen' component={MenuNavigatorScreen} options={{ title: 'Menu', headerShown: false }} />
+    </MainNavigator.Navigator>
+  );
+}
+
 class Main extends Component {
   render() {
     return (
       <NavigationContainer>
-        <MenuNavigatorScreen />
+        <MainNavigatorScreen />
       </NavigationContainer>
     );
   }
