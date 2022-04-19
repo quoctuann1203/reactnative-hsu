983e5a03ea84c455548f6534610e2bbfaa637e33
Quoc Tuan <you@example.com>
2022-04-19 10:24:29 +0700

    week 4 - assignment

---
+++ component/AboutComponent.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { Card, Divider, Image } from "react-native-elements";
+import { FlatList } from "react-native";
+import { ListItem, Avatar } from "react-native-elements";
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+
+
+class About extends Component {
+  render() {
+    {
+      const promotions = this.props.promotions;
+      const title = "Our History";
+      return (
+        <Card>
+          <Card.Title>{title}</Card.Title>
+          <Divider></Divider>
+          <Text style={{ margin: 10 }}>
+            Started in 2010, Ristorante con Fusion quickly established itself as
+            a culinary icon par excellence in Hong Kong. With its unique brand
+            of world fusion cuisine that can be found nowhere else, it enjoys
+            patronage from the A-list clientele in Hong Kong. Featuring four of
+            the best three-star Michelin chefs in the world, you never know what
+            will arrive on your plate the next time you visit us.{" "}
+          </Text>
+          <Text style={{ margin: 10 }}>
+            The restaurant traces its humble beginnings to The Frying Pan, a
+            successful chain started by our CEO, Mr. Peter Pan, that featured
+            for the first time the world's best cuisines in a pan.{" "}
+          </Text>
+          <FlatList
+            data={this.props.promotions}
+            renderItem={({ item, index }) => this.renderMenuItem(item, index)}
+            keyExtractor={(item) => item.id.toString()}
+          />;
+        </Card>
+      );
+    }
+  }
+  renderMenuItem(item, index) {
+    return (
+      <ListItem key={index}>
+        <Avatar source={require("./images/alberto.png")} />
+        <ListItem.Content>
+          <ListItem.Title>{item.name}</ListItem.Title>
+          <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  }
+}
+export default About;
--- component/MainComponent.js
+++ component/MainComponent.js
@@ -1,37 +1,78 @@
-import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import Home from './HomeComponent';
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Home from "./HomeComponent";
 function HomeNavigatorScreen() {
   const HomeNavigator = createStackNavigator();
   return (
     <HomeNavigator.Navigator
-      initialRouteName='Home'
+      initialRouteName="Home"
       screenOptions={{
-        headerStyle: { backgroundColor: '#512DA8' },
-        headerTintColor: '#fff',
-        headerTitleStyle: { color: '#fff' }
-      }}>
-      <HomeNavigator.Screen name='Home' component={Home} />
+        headerStyle: { backgroundColor: "#512DA8" },
+        headerTintColor: "#fff",
+        headerTitleStyle: { color: "#fff" },
+      }}
+    >
+      <HomeNavigator.Screen name="Home" component={Home} />
     </HomeNavigator.Navigator>
   );
 }
 
-import Menu from './MenuComponent';
-import Dishdetail from './DishdetailComponent';
+function ContactNavigatorScreen() {
+  const ContactNavigator = createStackNavigator();
+  return (
+    <ContactNavigator.Navigator
+      initialRouteName="Contact"
+      screenOptions={{
+        headerStyle: { backgroundColor: "#512DA8" },
+        headerTintColor: "#fff",
+        headerTitleStyle: { color: "#fff" },
+      }}
+    >
+      <ContactNavigator.Screen name="Contact" component={Contact} />
+    </ContactNavigator.Navigator>
+  );
+}
+
+function AboutNavigatorScreen() {
+  const AboutNavigator = createStackNavigator();
+  return (
+    <AboutNavigator.Navigator
+      initialRouteName="About"
+      screenOptions={{
+        headerStyle: { backgroundColor: "#512DA8" },
+        headerTintColor: "#fff",
+        headerTitleStyle: { color: "#fff" },
+      }}
+    >
+      <AboutNavigator.Screen name="About" component={About} />
+    </AboutNavigator.Navigator>
+  );
+}
+
+import Menu from "./MenuComponent";
+import Dishdetail from "./DishdetailComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
+
 function MenuNavigatorScreen() {
   const MenuNavigator = createStackNavigator();
   return (
     <MenuNavigator.Navigator
-      initialRouteName='Menu'
+      initialRouteName="Menu"
       screenOptions={{
-        headerStyle: { backgroundColor: '#512DA8' },
-        headerTintColor: '#fff',
-        headerTitleStyle: { color: '#fff' }
-      }}>
-      <MenuNavigator.Screen name='Menu' component={Menu} />
-      <MenuNavigator.Screen name='Dishdetail' component={Dishdetail} options={{ headerTitle: 'Dish Detail' }} />
+        headerStyle: { backgroundColor: "#512DA8" },
+        headerTintColor: "#fff",
+        headerTitleStyle: { color: "#fff" },
+      }}
+    >
+      <MenuNavigator.Screen name="Menu" component={Menu} />
+      <MenuNavigator.Screen
+        name="Dishdetail"
+        component={Dishdetail}
+        options={{ headerTitle: "Dish Detail" }}
+      />
     </MenuNavigator.Navigator>
   );
 }
@@ -39,9 +80,27 @@ function MenuNavigatorScreen() {
 function MainNavigatorScreen() {
   const MainNavigator = createDrawerNavigator();
   return (
-    <MainNavigator.Navigator initialRouteName='HomeScreen'>
-      <MainNavigator.Screen name='HomeScreen' component={HomeNavigatorScreen} options={{ title: 'Home', headerShown: false }} />
-      <MainNavigator.Screen name='MenuScreen' component={MenuNavigatorScreen} options={{ title: 'Menu', headerShown: false }} />
+    <MainNavigator.Navigator initialRouteName="HomeScreen">
+      <MainNavigator.Screen
+        name="HomeScreen"
+        component={HomeNavigatorScreen}
+        options={{ title: "Home", headerShown: false }}
+      />
+      <MainNavigator.Screen
+        name="MenuScreen"
+        component={MenuNavigatorScreen}
+        options={{ title: "Menu", headerShown: false }}
+      />
+      <MainNavigator.Screen
+        name="ContactScreen"
+        component={ContactNavigatorScreen}
+        options={{ title: "Contact Us", headerShown: false }}
+      />
+      <MainNavigator.Screen
+        name="AboutSreen"
+        component={AboutNavigatorScreen}
+        options={{ title: "About Us", headerShown: false }}
+      />
     </MainNavigator.Navigator>
   );
 }
